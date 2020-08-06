7db928e4cba2b0121e3f63e578d9c391e8c9f191
Matvey Sokolov <bezyn4uk@hotmail.com>
2020-08-06 11:41:08 +0300

    Add isDivisible Kata

--- CodeWarsKatas/Fundamentals.cs
+++ CodeWarsKatas/Fundamentals.cs
@@ -54,6 +54,11 @@ namespace CodeWarsKatas
         }
 
 
+        public static bool isDivisible(long n, long x, long y)
+        {
+            return n % x == 0 && n % y == 0;
+        }
+
 
     }
 }
