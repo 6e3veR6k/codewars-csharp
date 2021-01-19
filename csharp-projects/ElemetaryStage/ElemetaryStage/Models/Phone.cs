b695403efa89df3c9cbccda365fc883b32302d6e
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-01-19 13:02:17 +0200

    Add new method in Phone class

--- csharp-projects/ElemetaryStage/ElemetaryStage/Models/Phone.cs
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Models/Phone.cs
@@ -8,5 +8,17 @@ namespace ElemetaryStage.Models
     public class Phone : IPhone
     {
         public string FullNumber { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        private int arp = 10;
+
+        public int Age { get; private set; }
+        public int Arp { get; }
+
+
+        public void SetArp()
+        { 
+            this.arp = 10; 
+            this.Age = 12; 
+        }
     }
 }
