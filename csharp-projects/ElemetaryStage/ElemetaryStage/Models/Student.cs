611de8583fe31e0663238d9ad4fab93bd12e165a
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-01-20 22:09:17 +0200

    Add exercises from itvdn

---
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Models/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ElemetaryStage.Models
+{
+    public delegate void ShowMessage(string message);
+
+
+    public class Student
+    {
+        public void Move(int distance, ShowMessage method)
+        {
+            for(int i = 1; i <= distance; i++)
+            {
+                Thread.Sleep(1000);
+                method(string.Format("Rolling... Distance: {0}", i));
+            }
+        }
+
+
+        public int GetAge()
+        {
+            return 17;
+        }
+
+        
+        public string Grow(int years)
+        {
+            return null;
+        }
+
+
+        public void SetInfo(string name, int age)
+        {
+            
+        }
+
+
+        public void SetSchool(int id, string number)
+        {
+
+        }
+    }
+}
--- csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElemetaryStage.Models;
+using System;
 
 namespace ElemetaryStage
 {
@@ -7,10 +8,28 @@ namespace ElemetaryStage
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+            StringHelper helper = new StringHelper();
+
+            CountDelegate delegate1 = helper.GetCount;
+            CountDelegate delegate2 = helper.GetCountSymbolA;
+
+            string testingString = "LAMP";
+
+            Console.WriteLine(TestDelegate(delegate1, testingString));
+            Console.WriteLine(TestDelegate(delegate2, testingString));
+
+            Console.ReadLine();
         }
 
         public static int Sum(int a, int b) => a + b;
 
         public static double GetPerimeter(double firstSide, double secondSide) => 2 * (firstSide + secondSide);
+
+        public static int TestDelegate(CountDelegate method, string testString)
+        {
+            return method(testString);
+        }
     }
 }
