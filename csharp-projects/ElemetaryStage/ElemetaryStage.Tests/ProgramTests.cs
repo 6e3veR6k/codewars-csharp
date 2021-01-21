80c41cf1b925f7e4c8b79842423803f59fb171c8
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-01-21 10:40:33 +0200

    create branch develop

--- csharp-projects/ElemetaryStage/ElemetaryStage.Tests/ProgramTests.cs
+++ csharp-projects/ElemetaryStage/ElemetaryStage.Tests/ProgramTests.cs
@@ -10,31 +10,5 @@ namespace ElemetaryStage.Tests
         {
         }
 
-        [Test]
-        public void Sum_10plus20_returned30()
-        {
-            int a = 10;
-            int b = 20;
-            int expected = 30;
-
-            var result = Program.Sum(a, b);
-
-            Debug.WriteLine("Ok!");
-            Assert.AreEqual(expected, result, $"Are not equal: e --> {expected}, result = {result}");
-        }
-
-        [Test]
-        public void GetPerimeter_20_10_returned60()
-        {
-            var firstSide = 10.000001;
-            var secondSide = 20.99998;
-
-            var expected = 2 * (firstSide + secondSide);
-
-            var result = Program.GetPerimeter(firstSide, secondSide);
-
-
-            Assert.AreEqual(expected, result);
-        }
     }
 }
--- csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
@@ -9,27 +9,7 @@ namespace ElemetaryStage
         {
             Console.WriteLine("Hello World!");
 
-
-            StringHelper helper = new StringHelper();
-
-            CountDelegate delegate1 = helper.GetCount;
-            CountDelegate delegate2 = helper.GetCountSymbolA;
-
-            string testingString = "LAMP";
-
-            Console.WriteLine(TestDelegate(delegate1, testingString));
-            Console.WriteLine(TestDelegate(delegate2, testingString));
-
             Console.ReadLine();
-        }
-
-        public static int Sum(int a, int b) => a + b;
-
-        public static double GetPerimeter(double firstSide, double secondSide) => 2 * (firstSide + secondSide);
-
-        public static int TestDelegate(CountDelegate method, string testString)
-        {
-            return method(testString);
         }
     }
 }
