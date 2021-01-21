55623b946af3d9831010da0391a3b694763bea64
Matvey Sokolov <bezyn4uk@hotmail.com>
2021-01-21 15:47:10 +0200

    Create rectangle class

---
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Models/Rectangle.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+#region
+/*
+ * Требуется: Создать класс с именем Rectangle.
+ * В теле класса создать два поля, описывающие длины сторон  double side1, side2.
+ * Создать пользовательский конструктор  Rectangle(double side1, double side2) , в теле которого
+ * поля  side1 и side2 инициализируются значениями аргументов.
+ * Создать два метода, вычисляющие площадь прямоугольника -  double AreaCalculator() и периметр
+ * прямоугольника -  double PerimeterCalculator().
+ * Создать два свойства  double Area и double Perimeter с одним методом доступа get.
+ * Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит
+ * на экран периметр и площадь.
+ */
+#endregion
+
+namespace ElemetaryStage.Models
+{
+    public class Rectangle
+    {
+        private double firstSide;
+        public double FirstSide 
+        { 
+            get => firstSide;
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length of rectangle side cant be less than 0", nameof(FirstSide));
+                }
+                else
+                {
+                    firstSide = value;
+                }
+            }
+        }
+
+
+        private double secondSide;
+        public double SecondSide 
+        {
+            get => secondSide;
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length of rectangle side cant be less than 0", nameof(SecondSide));
+                }
+                else
+                {
+                    secondSide = value;
+                }
+            }
+        }
+
+
+        public double Area { get => AreaCalculator(); }
+        public double Perimeter { get => PerimeterCalculator(); }
+
+
+
+        public Rectangle(double firstSide, double secondSide)
+        {
+            FirstSide = firstSide;
+            SecondSide = secondSide;
+        }
+
+        private double AreaCalculator()
+        {
+            return FirstSide * SecondSide;
+        }
+
+        private double PerimeterCalculator()
+        {
+            return 2 * (FirstSide + SecondSide);
+        }
+
+    }
+}
--- csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
+++ csharp-projects/ElemetaryStage/ElemetaryStage/Program.cs
@@ -1,15 +1,88 @@
 ﻿using ElemetaryStage.Models;
 using System;
+using System.Globalization;
+
+#region
+/*
+ * Требуется: Создать класс с именем Rectangle.
+ * В теле класса создать два поля, описывающие длины сторон  double side1, side2.
+ * Создать пользовательский конструктор  Rectangle(double side1, double side2) , в теле которого
+ * поля  side1 и side2 инициализируются значениями аргументов.
+ * Создать два метода, вычисляющие площадь прямоугольника -  double AreaCalculator() и периметр
+ * прямоугольника -  double PerimeterCalculator().
+ * Создать два свойства  double Area и double Perimeter с одним методом доступа get.
+ * Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит
+ * на экран периметр и площадь.
+ */
+#endregion
 
 namespace ElemetaryStage
 {
+
+
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+            Console.WriteLine("Enter first side of the rectangle");
+            var firstSide = GetRectangleSide();
+
+            Console.WriteLine("Enter second side of the rectangle");
+            var secondSide = GetRectangleSide();
+
+            Rectangle rectangle = new Rectangle(firstSide, secondSide);
+
+            Console.WriteLine($"Rectangle with sides: {firstSide}X{secondSide} has area = {rectangle.Area} and perimeter = {rectangle.Perimeter};");
+
             Console.ReadLine();
         }
+
+        private static double GetRectangleSide()
+        {
+            double side;
+
+            while (true)
+            {
+                side = GetUserInputValue();
+
+                if (side <= 0)
+                {
+                    Console.WriteLine($"Side cann't be negative or zero: {side}");
+                    continue;
+                }
+
+                break;
+            }
+
+            return side;
+        }
+
+
+        private static double GetUserInputValue()
+        {
+            double userValue;
+            while (true)
+            {
+                var userInput = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Input cannot be empty, try again.");
+                    continue;
+                }
+
+                if (!double.TryParse(userInput, NumberStyles.Float | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out userValue))
+                {
+                    Console.WriteLine("Input cannot be parsed, try again.");
+                    continue;
+                }
+
+                break;
+            }
+
+            return userValue;
+        }
     }
+
 }
